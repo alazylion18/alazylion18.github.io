a64ae2014708c9c37ab0867656890beecf03a6c0
Libraries <libraries@LIBRLTMCK220837.AD.UMD.EDU>
2019-10-15 19:32:01 -0400

    Leonardo Giovanini developed new approach to development. Instead of using two functions, a script will convert the json object into an array, so that a for loop extracts the desired elements using the index from the combinationArray as the index for the array of country names. Solution development is in progress. Code testing and quality testing will happen after solution development.

--- lab_5/myScript.js
+++ lab_5/myScript.js
@@ -245,6 +245,35 @@ let countries = [
   { "name": "Zimbabwe", "code": "ZW" }
 ]
 
+let arrayOfCountries = ["Afghanistan",  "Åland Islands", "Albania", "Algeria", "American Samoa",  "Andorra", "Angola", "Anguilla", 
+"Antarctica", "Antigua and Barbuda", "Argentina", "Armenia", "Aruba", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", 
+"Bangladesh", "Barbados", 'Belarus', "Belgium", "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia", "Bosnia and Herzegovina", 
+"Botswana", "Bouvet Island", "Brazil", "British Indian Ocean Territory", "Brunei Darussalam", 'Bulgaria', "Burkina Faso", "Burundi", 
+"Cambodia", "Cameroon", "Canada", "Cape Verde", "Cayman Islands", "Central African Republic", "Chad", "Chile", "China", "Christmas Island",
+"Cocos (Keeling) Islands", "Colombia", "Comoros", "Congo", "Congo, The Democratic Republic of the", "Cook Islands", "Costa Rica, Cote D`Ivoire", 
+"Croatia", "Cuba", "Cyprus", "Czech Republic", "Denmark", "Djibouti", "Dominica", "Dominican Republic", "Ecuador", "Egypt",  "El Salvador",
+ "Equatorial Guinea", "Eritrea", "Estonia", "Ethiopia", "Falkland Islands (Malvinas)", "Faroe Islands", "Fiji", "Finland", "France", 
+ "French Guiana", "French Polynesia", "French Southern Territories", "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Gibraltar",
+  "Greece", "Greenland", "Grenada", "Guadeloupe", "Guam", "Guatemala", "Guernsey", "Guinea", "Guinea-Bissau", "Guyana", "Haiti", 
+  "Heard Island and Mcdonald Islands", "Holy See (Vatican City State)", "Honduras", "Hong Kong", "Hungary", "Iceland", "India", "Indonesia",
+   "Iran", "Islamic Republic Of, Iraq", "Ireland", "Isle of Man", "Israel", "Italy", "Jamaica", "Japan", "Jersey", "Jordan", "Kazakhstan", 
+   "Kenya", "Kiribati", "Korea, Democratic People's Republic of", "Korea, Republic of", "Kuwait", "Kyrgyzstan", "Lao People's Democratic Republic", 
+   "Latvia", "Lebanon", "Lesotho", "Liberia", "Libyan Arab Jamahiriya", "Liechtenstein", "Lithuania", "Luxembourg", "Macao", "Macedonia, The Former Yugoslav Republic of", 
+   "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta",  "Marshall Islands", "Martinique", "Mauritania", "Mauritius", "Mayotte", 
+   "Mexico", "Micronesia, Federated States of", "Moldova, Republic of", "Monaco", "Mongolia", "Montserrat", "Morocco", "Mozambique", "Myanmar", 
+   "Namibia", "Nauru", "Nepal", "Netherlands", "Netherlands, Antilles", "New Caledonia", "New Zealand", "Nicaragua",  "Niger", "Nigeria", 
+   "Niue", "Norfolk Island", "Northern Mariana Islands", "Norway", "Oman", "Pakistan", "Palau", "Palestinian Territory, Occupied", "Panama", 
+   "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Pitcairn", "Poland", "Portugal", "Puerto Rico", "Qatar", "Reunion", "Romania", 
+   "Russian Federation", "RWANDA", "Saint Helena", "Saint Kitts and Nevis", "Saint Lucia", "Saint Pierre and Miquelon", "Saint Vincent and the Grenadines", 
+   "Samoa", "San Marino", "Sao Tome and Principe", "Saudi Arabia", "Senegal", "Serbia and Montenegro", "Seychelles", "Sierra Leone", 
+   "Singapore", "Slovakia", "Slovenia", "Solomon Islands", "Somalia", "South Africa", "South Georgia and the South Sandwich Islands", 
+   "Spain", "Sri Lanka", "Sudan", "Suriname", "Svalbard and Jan Mayen", "Swaziland", "Sweden", "Switzerland", "Syrian Arab Republic", "Taiwan, Province of China", 
+   "Tajikistan", "Tanzania, United Republic of", "Thailand", "Timor-Leste", "Togo", "Tokelau", "Tongo", "Trinidad and Tobago", 
+   "Tunisia", "Turkey", "Turkmenistan", "Turks and Caicos Islands", "Tuvalu", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", 
+   "United States", "United States Minor Outlying Islands", "Uruguay", "Uzbekistan", "Vanuatu", "Venezuela", "Viet Nam", "Virgin Islands, British", 
+  "Virgin Islands, U.S.", "Wallis and Futuna", "Western Sahara", "Yemen", "Zambia", "Zimbabwe"]
+
+
 // Initializes numberCombination array object of 50 elements set to -1
 let numberCombination = new Array (50).fill('-1');
 
@@ -316,6 +345,14 @@ function compareCombinations(){
 }
 
 
+let countriesArray = new Array(50)
+
+for (let i=0; i<=50; i++){
+  combinationNumber = sortedCombination[i].toFixed(0);
+  countriesArray[i] = combinationNumber;
+}
+
+
 function editPage(){
   document.getElementById("instructions").innerHTML = countries.map(getCountry);
 }
@@ -323,12 +360,8 @@ function editPage(){
 
 function printCombination() {
   currentCombination = compareCombinations();
-  let htmlCode = ''
-  for (let i = 0; i <= currentCombination.length; i++)
-  {
-    htmlCode = htmlCode + 
-
-  }
+  window.alert(countriesArray)
+  //let htmlCode = ''
   window.alert(currentCombination);
 }
 
